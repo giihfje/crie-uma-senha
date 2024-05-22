aafedfbc6a57038a59a4cb6224c4be66f3b75716
gisele <108832972+giihfje@users.noreply.github.com>
2024-05-22 15:04:49 -0300

    2205

--- main.js
+++ main.js
@@ -7,13 +7,27 @@ botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
     if(tamanhoSenha > 1){
-    tamanhoSenha = tamanhoSenha - 1;
+// tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
-    tamanhoSenha = tamanhoSenha + 1;
+// tamanhoSenha = tamanhoSenha + 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
+// codig omitido
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWYZ';
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++){
+    let numeroAletorio = Math.random()*letrasMaiusculas.length;
+    numeroAletorio = Math.floor(numeroAletorio);
+    senha = senha + letrasMaiusculas[numeroAletorio];
+    }
+}
+campoSenha.value = senha;
