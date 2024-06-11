1214d6cd65f6b7862723b203ca44c89142a53835
gisele <108832972+giihfje@users.noreply.github.com>
2024-06-11 11:24:59 -0300

    1106

--- main.js
+++ main.js
@@ -29,7 +29,6 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
 const simbolos = '!@%*?';
-const valorEntropia = document.querySelector('.entropia');
 function geraSenha(){
     let alfabeto = '';
     if (checkbox[0].checked){
@@ -53,15 +52,16 @@ function geraSenha(){
     campoSenha.value = senha;
     classificaSenha(alfabeto.length);
     }
-    function classificaSenha(tamanhoAlfabeto){
-        let entropia = tamanhoSenha * Math.log2(alfabeto.length);
-        forcaSenha.classList.remove('fraca','media','forte');
-        if (entropia> 57){
-            forcaSenha.classList.add('forte');
-        } else if (entropia > 35 && entropia < 57){
-            forcaSenha.classList.add('media');
-        } else if (entropia < 35){
-            forcaSenha.classList.add('fraca');
-        }
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (entropia > 57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57){
+        forcaSenha.classList.add('media');
+    } else if (entropia < 35){
+        forcaSenha.classList.add('fraca');
     }
-    valorEntropia.textContent = "um conputador pode levar ate"+ Math.floor(2**entropia)/(100e6*60*60*24);+"dias para descobrir a senha";
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+}
